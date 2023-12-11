24e092bf7092c9c57377dda32d1322087c899b7e
Theo.bsl <t.basile@lahorde.school>
2023-12-11 20:46:39 +0100

    lore ve and fading out

--- My project/Assets/NarratifSystem/Script/NarratifManager.cs
+++ My project/Assets/NarratifSystem/Script/NarratifManager.cs
@@ -1,18 +1,27 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq.Expressions;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class NarratifManager : MonoBehaviour
 {
-    private TextMeshProUGUI _text;
-    private TextMeshProUGUI _dialogue;
-    private Image _face;
+    [SerializeField] private TextMeshProUGUI _text;
+    [SerializeField] private TextMeshProUGUI _dialogue;
+    [SerializeField] private Image _face;
+    [SerializeField] private Image _background;
 
+    [SerializeField] private float _time = 0;
+    [SerializeField] private int _waitTime = 2;
+    [SerializeField] private int _speed = 1;
+
+    private bool _canPassText = true;
     private int _index = 0;
 
-    private List<string> _introTexts = new List<string>()
+    #region vf
+    /*private List<string> _introTexts = new List<string>()
     {
         "Dans un monde où la magie et les monstres sont omniprésents, un homme mystérieux cherchait à recruter de courageux aventuriers." +
         "\n\n\rSa quête : récupérer un ancien et puissant artéfact situé dans les ruines d’une ancienne civilisation : le catalyseur d’Eldritch.",
@@ -66,7 +75,67 @@ public class NarratifManager : MonoBehaviour
         "veillant ainsi à ce qu'aucun autre être humain ne puisse pénétrer en ces lieux mystérieux.\r\n"
     };
     private List<string> _badEndTexts = new List<string>()
-    { "Malgré leurs efforts et leurs ténacités, les aventuriers ne parvinrent pas à surpasser la puissance de l’homme mystérieux."};
+    { "Malgré leurs efforts et leurs ténacités, les aventuriers ne parvinrent pas à surpasser la puissance de l’homme mystérieux."};*/
+    #endregion
+
+    #region ve
+    private List<string> _introTexts = new List<string>()
+    {
+        "In a world where magic and fantastic creatures are omnipresent, a mysterious being with a frail, weather-beaten physique was striving to rally intrepid adventurers to his cause. " +
+        "\n\n\rHis quest: to find an ancient and powerful artifact, the mysterious Eldritch Catalyst, hidden among the remains of an ancient civilization now extinct.",
+
+        "It was in a tavern with a suspicious aura that he crossed paths with four adventurers: " +
+        "\n\rMagnus Stormblade, a brute force; Lila Nightshade, a greedy thief; " +
+        "\n\rElara Moonfire, a witch with modest powers; and Thaddeus Emberstone, an aging alchemist. " +
+        "\n\n\rThe four adventurers, initially hesitant, gave in to the mysterious being's proposal when he offered the promise of money and power.",
+
+        "As Thaddeus Emberstone entered the dungeon, he felt an oppressive heaviness permeate the atmosphere, draining the energy from every member of the group."
+    };
+    private List<string> _gameTexts = new List<string>()
+    {
+        "Mysterious Being :",
+        "Well done, adventurers, you have reached the end of the dungeon.",
+
+        "Magnus Stormblade :",
+        "But... what are you doing here ?",
+
+        "Thaddeus Emberstone :",
+        "It was obviously a trap, wasn't it ?",
+
+        "Mysterious Being :",
+        "That’s right.",
+
+        "Elara Moonfire :",
+        "What do you plan to make us endure ?",
+
+        "Mysterious Being :",
+        "I'm going to steal all your life energy. ",
+
+        "Mysterious Being :",
+        "Behold this magnificent artifact, the Eldritch Catalyst; " +
+        "it absorbs the life energy of the humans present in this place and transfers it to its owner... me.",
+
+        "Lila Nightshade :",
+        "But why are you doing this ?",
+
+        "Mysterious Being :",
+        "For eternal life, what a question.",
+
+        "Magnus Stormblade :",
+        "You used us. I'm going to massacre you.",
+
+        "Mysterious Being :",
+        "Go ahead, come and try your luck !"
+    };
+    private List<string> _goodEndTexts = new List<string>()
+    {
+        "Despite the formidable power of their adversary, the adventurers emerged triumphant from this arduous battle. " +
+        "Once they had recovered from their wounds, they decided to destroy the cursed artifact and seal the entrance to the dungeon forever, " +
+        "ensuring that no other human being would be able to enter."
+    };
+    private List<string> _badEndTexts = new List<string>()
+    { "Despite their best efforts and unfailing tenacity, the adventurers were unable to overcome the mysterious power of their opponents."};
+    #endregion
 
     [SerializeField] private List<Sprite> _faces = new List<Sprite>(); //warrior, thief, witch, alchimist, 
 
@@ -77,15 +146,6 @@ public class NarratifManager : MonoBehaviour
 
     public Phase _phase = Phase.Intro;
 
-    private void Awake()
-    {
-        Transform[] children = GetComponentsInChildren<Transform>(true);
-
-        _text = children[0].GetComponent<TextMeshProUGUI>();
-        _dialogue = children[1].GetComponent<TextMeshProUGUI>();
-        _face = children[2].GetComponent<Image>();
-    }
-
     private void Start()
     {
         _text.text = _introTexts[_index++];
@@ -93,15 +153,18 @@ public class NarratifManager : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && _canPassText)
         {
             switch (_phase)
             {
                 case Phase.Intro:
                     if (_index == _introTexts.Count)
                     {
+                        _canPassText = false;
+                        StartCoroutine(DesableBackground());
                         //gamemanager launch game
                         Debug.Log("end intro");
+
                     }
                     else
                     {
@@ -112,8 +175,8 @@ public class NarratifManager : MonoBehaviour
                 case Phase.BeforeBossFight:
                     if (_index == _gameTexts.Count)
                     {
-                        _dialogue.enabled = false;
-                        _face.enabled = false;
+                        _canPassText = false;
+                        DesableDialogue();
                         //gamemanager launch boss fight
                         Debug.Log("end boss fight");
                     }
@@ -127,6 +190,7 @@ public class NarratifManager : MonoBehaviour
                 case Phase.GoodEnd:
                     if (_index == _goodEndTexts.Count)
                     {
+                        _canPassText = false;
 
                         Debug.Log("end good end");
                     }
@@ -139,6 +203,7 @@ public class NarratifManager : MonoBehaviour
                 case Phase.BadEnd:
                     if (_index == _badEndTexts.Count)
                     {
+                        _canPassText = false;
 
                         Debug.Log("end bad end");
                     }
@@ -156,23 +221,15 @@ public class NarratifManager : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            ChangePhase(Phase.BeforeBossFight);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            ChangePhase(Phase.GoodEnd);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            ChangePhase(Phase.BadEnd);
+            NextNarratifPhase();
         }
     }
 
-    public void ChangePhase(Phase phase)
+    public void NextNarratifPhase()
     {
-        _phase = phase;
+        _phase = Enum.GetValues(typeof(Phase)).Cast<Phase>().SkipWhile(e => e != _phase).Skip(1).First();
+
+        _canPassText = true;
         _index = 0;
 
         switch (_phase)
@@ -187,11 +244,15 @@ public class NarratifManager : MonoBehaviour
                 break;
 
             case Phase.GoodEnd:
+                EnableNarratif();
+
                 _text.enabled = true;
                 _text.text = _goodEndTexts[_index++];
                 break;
 
             case Phase.BadEnd:
+                EnableNarratif();
+
                 _text.enabled = true;
                 _text.text = _badEndTexts[_index++];
                 break;
@@ -206,7 +267,7 @@ public class NarratifManager : MonoBehaviour
     {
         switch (_gameTexts[_index++])
         {
-            case "Homme mystérieux :":
+            case "Mysterious Being :":
                 _face.sprite = _faces[4];
                 break;
 
@@ -222,7 +283,7 @@ public class NarratifManager : MonoBehaviour
                 _face.sprite = _faces[2];
                 break;
 
-            case "Thaddeus Emberstone":
+            case "Thaddeus Emberstone :":
                 _face.sprite = _faces[3];
                 break;
 
@@ -230,5 +291,36 @@ public class NarratifManager : MonoBehaviour
                 Debug.Log("Error wrong name");
                 break;
         }
+    }
+
+    private IEnumerator DesableBackground()
+    {
+        Color fromBg = Color.black;
+        Color toBg = Color.clear;
+
+        Color fromTxt = Color.white;
+        Color toTxt = Color.white - Color.black;
+
+        _time = 0;
+
+        while (_time < 2)
+        {
+            _background.color = Color.Lerp(fromBg, toBg, _time);
+            _text.color = Color.Lerp(fromTxt, toTxt, _time);
+            _time += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    private void DesableDialogue()
+    {
+        _face.enabled = false;
+        _dialogue.enabled = false;
+    }
+
+    private void EnableNarratif()
+    {
+        _background.color = Color.black; 
+        _text.color = Color.white;
     }
 }
