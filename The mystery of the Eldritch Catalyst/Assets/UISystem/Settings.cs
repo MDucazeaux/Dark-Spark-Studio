495d0a6d69f57387d843c49db97360cf4e668cb1
UnPseudoTotalementNormal <azertizeryt@gmail.com>
2023-12-19 15:13:59 +0100

    Settings (fullscreen/resolution)

--- The mystery of the Eldritch Catalyst/Assets/UISystem/Settings.cs
+++ The mystery of the Eldritch Catalyst/Assets/UISystem/Settings.cs
@@ -1,4 +1,5 @@
 using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -15,7 +16,8 @@ public class Settings : MonoBehaviour
     [SerializeField] private Slider _sfxVolSlider;
     [SerializeField] private Slider _musicVolSlider;
 
-    [SerializeField] private Image _blackFilter;
+    [SerializeField] private TMP_Dropdown _resolutionDropdown;
+    [SerializeField] private Toggle _fullscreenToggle;
 
     private float _mainVolume;
     private float _sfxVolume;
@@ -32,6 +34,8 @@ public class Settings : MonoBehaviour
         _mainVolSlider.onValueChanged.AddListener(delegate { OnMainVolumeChanged(); });
         _sfxVolSlider.onValueChanged.AddListener(delegate { OnSfxVolumeChanged(); });
         _musicVolSlider.onValueChanged.AddListener(delegate { OnMusicVolumeChanged(); });
+        _resolutionDropdown.value = 3;
+        _fullscreenToggle.isOn = Screen.fullScreen;
         DontDestroyOnLoad(_canvas);
         gameObject.SetActive(false);
     }
@@ -48,7 +52,26 @@ public class Settings : MonoBehaviour
 
     public void OnFullscreen(bool fullscreen)
     {
+        Screen.fullScreen = fullscreen;
+    }
 
+    public void OnResolutionChange()
+    {
+        switch (_resolutionDropdown.value)
+        {
+            case 0:
+                Screen.SetResolution(640, 360, Screen.fullScreen);
+                break;
+            case 1:
+                Screen.SetResolution(1280, 720, Screen.fullScreen);
+                break;
+            case 2:
+                Screen.SetResolution(1920, 1080, Screen.fullScreen);
+                break;
+            case 3:
+                Screen.SetResolution(2560, 1440, Screen.fullScreen);
+                break;
+        }
     }
 
     public void OnMainVolumeChanged()
