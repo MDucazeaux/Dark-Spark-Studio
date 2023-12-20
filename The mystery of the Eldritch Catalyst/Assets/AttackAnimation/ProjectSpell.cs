dbb2d8065941f2d866a3933b862109ae3054b71d
UnPseudoTotalementNormal <azertizeryt@gmail.com>
2023-12-20 10:23:05 +0100

    added transmutation sprite

---
+++ The mystery of the Eldritch Catalyst/Assets/AttackAnimation/ProjectSpell.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class ProjectSpell : MonoBehaviour
+{
+    private Vector3 _startingPos;
+    [SerializeField] private float _travelDistance;
+    [SerializeField] private float _travelSpeed;
+
+    void Update()
+    {
+        
+    }
+}
